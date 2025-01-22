03567edb7a0f7524bef8ca324fac29938a2f3968
jon <jon@ideograph.com>
2025-01-22 08:51:49 -0600

    tweak: call the gameloop from the playfield package

--- cmd/avoid_space_rocks/main.go
+++ cmd/avoid_space_rocks/main.go
@@ -1,24 +1,8 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
-
-const (
-	screenWidth  = 800
-	screenHeight = 450
-)
+import "avoid_the_space_rocks/internal/scenes/playfield"
 
 func main() {
-	rl.InitWindow(screenWidth, screenHeight, "Avoid the Space Rocks")
-	defer rl.CloseWindow()
-
-	rl.SetTargetFPS(60)
-
-	for !rl.WindowShouldClose() {
-		rl.BeginDrawing()
-
-		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("Avoid the Space Rocks", 190, 200, 20, rl.Black)
-
-		rl.EndDrawing()
-	}
+	// For now there's only one screen
+	playfield.GameLoop()
 }
