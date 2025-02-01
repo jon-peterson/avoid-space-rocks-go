38c91b3ffeab5040a4a2a03d12006a6a97d78678
jon <jon@ideograph.com>
2025-02-01 10:33:26 -0600

    tests: initial tests on the game object

---
+++ internal/scenes/playfield/game_test.go
@@ -0,0 +1,36 @@
+package playfield
+
+import (
+	"testing"
+)
+
+func TestInitGame(t *testing.T) {
+	world := World{} // Assuming World is a struct that can be initialized like this
+	game := InitGame(world)
+
+	if game.Lives != 3 {
+		t.Errorf("Expected Lives to be 3, got %d", game.Lives)
+	}
+
+	if game.Level != 1 {
+		t.Errorf("Expected Level to be 1, got %d", game.Level)
+	}
+
+	if game.Score != 0 {
+		t.Errorf("Expected Score to be 0, got %d", game.Score)
+	}
+
+	if game.World != world {
+		t.Errorf("Expected World to be initialized correctly")
+	}
+}
+
+func TestGetGame(t *testing.T) {
+	world := World{}
+	InitGame(world)
+
+	game := GetGame()
+	if game == nil {
+		t.Fatal("Expected game to be initialized, got nil")
+	}
+}
