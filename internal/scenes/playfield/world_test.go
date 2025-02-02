6b7256fcb474faf72dcdfccf261245c10d002de8
jon <jon@ideograph.com>
2025-02-02 15:04:08 -0600

    test: for wraparound fn

---
+++ internal/scenes/playfield/world_test.go
@@ -0,0 +1,27 @@
+package playfield
+
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+	"testing"
+)
+
+func TestWraparound(t *testing.T) {
+	world := NewWorld(800, 600)
+
+	tests := []struct {
+		input    rl.Vector2
+		expected rl.Vector2
+	}{
+		{input: rl.Vector2{X: -10, Y: 300}, expected: rl.Vector2{X: 800, Y: 300}},
+		{input: rl.Vector2{X: 810, Y: 300}, expected: rl.Vector2{X: 0, Y: 300}},
+		{input: rl.Vector2{X: 400, Y: -10}, expected: rl.Vector2{X: 400, Y: 600}},
+		{input: rl.Vector2{X: 400, Y: 610}, expected: rl.Vector2{X: 400, Y: 0}},
+	}
+
+	for _, test := range tests {
+		result := world.Wraparound(test.input)
+		if result != test.expected {
+			t.Errorf("Wraparound(%v) = %v, want %v", test.input, result, test.expected)
+		}
+	}
+}
