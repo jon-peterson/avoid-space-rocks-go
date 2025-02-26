0a0463a8508c2c1dcea6ce66106611bd79e22c35
jon <jon@ideograph.com>
2025-02-26 04:45:03 -0600

    feat: use Orbitron font

--- internal/utils/text.go
+++ internal/utils/text.go
@@ -1,14 +1,31 @@
 package utils
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+	"sync"
+)
+
+const spacing = 2.0
+
+var font rl.Font
+var once sync.Once
+
+func getFont() *rl.Font {
+	once.Do(func() {
+		font = rl.LoadFontEx("assets/fonts/Orbitron-Regular.ttf", 32, nil, 250)
+	})
+	return &font
+}
 
 // CenterText draws the given text centered around the passed-in position
 func CenterText(text string, position rl.Vector2, fontSize int32) {
-	textSize := rl.MeasureText(text, fontSize)
-	pos := rl.Vector2{X: position.X - float32(textSize)/2, Y: position.Y - float32(fontSize)/2}
+	font := getFont()
+	textSize := rl.MeasureTextEx(*font, text, float32(fontSize), spacing)
+	pos := rl.Vector2{X: position.X - textSize.X/2, Y: position.Y - textSize.Y/2}
 	WriteText(text, pos, fontSize)
 }
 
 func WriteText(text string, position rl.Vector2, fontSize int32) {
-	rl.DrawText(text, int32(position.X), int32(position.Y), fontSize, rl.Black)
+	font := getFont()
+	rl.DrawTextEx(*font, text, position, float32(fontSize), spacing, rl.Black)
 }
