f6c33582267d097518fbdee9a4373f47e6ac0fd7
jon <jon@ideograph.com>
2025-03-04 09:03:06 -0600

    feat: some graphics flair for the hyperspace feature

--- internal/scenes/playfield/gamewarden.go
+++ internal/scenes/playfield/gamewarden.go
@@ -66,11 +66,35 @@ func (gw *GameWarden) SpaceshipDestroyedWatcher() {
 	}
 }
 
-// SpaceshipHyperspaceWatcher moves the spaceship to a random location with some graphic flair
+// SpaceshipHyperspaceWatcher moves the spaceship to a random location with some graphic flair.
+// The audio clip is two seconds but the re-entry clack is at 1.9 seconds, so adjust accordingly.
 func (gw *GameWarden) SpaceshipHyperspaceWatcher() {
 	s := &gw.game.World.Spaceship
+	// Stop the spaceship and put it in hyperspace
 	s.InHyperspace = true
-	time.Sleep(4 * time.Second)
+	s.Velocity = rl.Vector2{}
+	s.Acceleration = rl.Vector2{}
+	// Send four pieces of the spaceship to random locations
+	pieces := make([]*core.Shrapnel, 4)
+	for i := 0; i < 4; i++ {
+		piece := core.NewShrapnel(s.Position, s.Spritesheet, 1800, i+3)
+		pos := gw.game.World.RandomPosition()
+		piece.Velocity = rl.Vector2Normalize(rl.Vector2Subtract(pos, piece.Position))
+		piece.Velocity = rl.Vector2Scale(piece.Velocity, 300)
+		pieces[i] = &piece
+		gw.game.World.Objects.Add(&piece)
+	}
+	time.Sleep(900 * time.Millisecond)
+	// Place the spaceship at a random location, and send the four pieces to its new location
 	s.Position = gw.game.World.RandomPosition()
+	for i := 0; i < 4; i++ {
+		// Create a vector that points from piece.Position to s.Position
+		piece := &pieces[i]
+		(*piece).Velocity = rl.Vector2Normalize(rl.Vector2Subtract(s.Position, (*piece).Position))
+		// Scale the velocity so it arrives at the new space position
+		distance := rl.Vector2Distance(s.Position, (*piece).Position)
+		(*piece).Velocity = rl.Vector2Scale((*piece).Velocity, distance)
+	}
+	time.Sleep(time.Second)
 	s.InHyperspace = false
 }
